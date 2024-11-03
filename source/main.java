56e5021f1b974e423489489c36dea12535c385eb
BdAbiy <badr22hari@gmail.com>
2024-11-03 09:59:19 +0100

    hh

--- source/main.java
+++ source/main.java
@@ -3,9 +3,8 @@ public class main {
 
     public static void main(String[] args) {
         try {
-        RequestReader r = new RequestReader("GET HTTP/1.1");
-        System.out.println(r.contenttype+"\n"+r.httpvers+"\n"+r.methode+"\n"+r.path);
-       }catch(Exception e){
+        System.out.println(0.2 +0.4);
+        }catch(Exception e){
         e.printStackTrace();
         
        }}
